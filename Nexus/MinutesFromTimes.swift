772fb2a0dc53fee855bf1748abbb9b3f99f68776
cliffdawg <cliffordyin@gmail.com>
2018-06-12 21:49:53 -0700

    Implemented Hero and editing Core Data

---
+++ Nexus/MinutesFromTimes.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Nexus
+//
+//  Created by Clifford Yin on 6/2/18.
+//  Copyright © 2018 Clifford Yin. All rights reserved.
+//
+
+import Foundation
---
+++ Nexus/ViewControllers/BoardViewController/Models/DownloadItem.swift
@@ -0,0 +1,9 @@
+//
+//  DownloadItem.swift
+//  Nexus
+//
+//  Created by Clifford Yin on 5/31/18.
+//  Copyright © 2018 Clifford Yin. All rights reserved.
+//
+
+import Foundation
